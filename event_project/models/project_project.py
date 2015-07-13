443f6ca2b69b279224c25a5bfbc3c33472441a57
Antonio Espinosa <antespi@gmail.com>
2015-07-13 17:20:20 +0200

    [FIX] Views bugfixes

--- event_project/models/project_project.py
+++ event_project/models/project_project.py
@@ -3,7 +3,11 @@
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
+from openerp import models, fields
 
-from . import event_event
-from . import project_project
-from . import project_task
+
+class Project(models.Model):
+    _inherit = 'project.project'
+
+    event = fields.One2many(comodel_name='event.event',
+                            inverse_name='project', string='Related event')
